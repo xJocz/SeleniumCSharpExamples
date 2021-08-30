941de52efb9e8ae09e1c4c83ad285ab197ace61c
Gabriel Leme <gabriel@easycomtec.com>
2021-08-29 21:39:00 -0300

    Created driver factory

---
+++ SeleniumCSharpExamples/Factory/DriverFactory.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpExamples.Factory
+{
+    class DriverFactory
+    {
+        private static IWebDriver driver;
+
+        private DriverFactory()
+        {
+
+        }
+
+        public static IWebDriver getDriver()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+            }
+            return driver;
+        }
+
+        public static void killDriver()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver = null;
+            }
+        }
+    }
+}
